02b991c6874a3319ac2169f9449521c2a18fb760
Baozhu Zuo <zuobaozhu@gmail.com>
2021-01-14 08:35:49 +0800

    add sdram1 mpu configures

--- variants/Seeeduino_H7AI/variant.cpp
+++ variants/Seeeduino_H7AI/variant.cpp
@@ -194,6 +194,76 @@ WEAK void SystemClock_Config(void)
   }
 }
 
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  Configure the MPU attributes.
+  * @note   The Base Address 0x24000000 is the SRAM1 accessible by the SDIO internal DMA.
+            the configured region is 512Kb size.
+  * @param  None
+  * @retval None
+  */
+static void MPU_Config(void)
+{
+  MPU_Region_InitTypeDef MPU_InitStruct;
+
+  /* Disable the MPU */
+  HAL_MPU_Disable();
+
+  /* Configure the MPU attributes as Normal Non Cacheable for SRAM1 */
+
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.BaseAddress = 0x24000000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_512KB;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER1;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.SubRegionDisable = 0x00;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+
+  /* Enable the MPU */
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
+}
+
+
+
+/**
+  * @brief  This function performs the global init of the system (HAL, IOs...)
+  * @param  None
+  * @retval None
+  */
+void init(void)
+{
+  /* MPU Configuration--------------------------------------------------------*/
+  MPU_Config();
+
+  /* Enable I-Cache---------------------------------------------------------*/
+  SCB_EnableICache();
+
+  /* Enable D-Cache---------------------------------------------------------*/
+  SCB_EnableDCache();
+
+  /* Initialize the HAL */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+#if defined (USBCON) && defined(USBD_USE_CDC)
+  USBD_CDC_init();
+#endif
+}
+
+
 #ifdef __cplusplus
 }
 #endif
